99e8d3544fc68f618a72f24d10495ff910635e5d
Dhakshina Moorthi <75572525+DhakshinaMoorthi-Techee@users.noreply.github.com>
2022-07-28 15:02:57 +0530

    Initial commit

---
+++ src/demo.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import 'antd/dist/antd.css';
+import './index.css';
+import { Input } from 'antd';
+
+
+const App = () => <Input placeholder="Basic usage" />;
+
+export default App;
